111e2a13cbc4a24d4c841004c72ac765d0db4b42
LB <lbederov@gmail.com>
2025-02-13 00:50:40 +0000

    Updates for deployment on GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+//const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  //basePath: isProd ? '/flashy' : '',
+ // distDir: 'dist', // or any other name you prefer
+ output: 'export',
+ assetPrefix: process.env.ASSET_PREFIX,
+ basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
