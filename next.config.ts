9d484b500fe740329d05f99092d5030bf8a9c5cb
LB <lbederov@gmail.com>
2025-02-18 17:54:58 +0000

    added static version for hosting

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   //basePath: isProd ? '/flashy' : '',
- // distDir: 'dist', // or any other name you prefer
+ distDir: 'docs', // or any other name you prefer
  output: 'export',
- assetPrefix: process.env.ASSET_PREFIX,
- basePath: process.env.BASE_PATH,
+ //basePath: '/docs',
+ //assetPrefix: process.env.ASSET_PREFIX,
+ //basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
